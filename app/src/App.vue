bf8cc4bb0c29f57d4171903460ae99f071870080
max long <long.maxime@gmail.com>
2017-04-26 16:33:47 -0700

    grid stuff

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,11 +1,15 @@
 <template>
   <div id="app">
-    <div class="margin-container">
-      <app-menu></app-menu>
+    <div class="fixed-top">
+      <div class="margin-container">
+        <app-menu></app-menu>
+      </div>
     </div>
 
-    <div class="margin-container view-container">
-      <router-view></router-view>
+    <div class="margin-container body" :class="'body-' + routeName">
+      <div class="view-container" :class="'view-' + routeName">
+        <router-view></router-view>
+      </div>
     </div>
 
   </div>
@@ -16,13 +20,15 @@ module.exports =
   name: 'app',
   components:
     appMenu: require('components/appMenu')
+
+  computed:
+    routeName: -> return @$store.state.route.name.toLowerCase()
+
 </script>
 
 <style lang="sass">
 @import src/styles/main
 #app
   +defaultType(normal)
-  .view-container
-    height: calc(100vh - 70px)
 
 </style>
