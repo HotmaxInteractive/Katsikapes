879d0a508e6ac2866bbaf8b6fad14b9b6228ca36
max long <long.maxime@gmail.com>
2017-05-07 13:00:12 -0700

    landing fix

--- app/src/views/v_landing.vue
+++ app/src/views/v_landing.vue
@@ -1,13 +1,13 @@
 <template>
   <div id="v_landing">
 
-    <div class="image-container">
-      <div class="link-container">
-        <div class="link-header">Some tagline that we have to come up with</div>
-        <div class="link-buttons">
-          <div class="button">See our Services</div>
-          <div class="button">See our past Work</div>
-        </div>
+    <div class="imageContainer"></div>
+
+    <div class="linkContainer">
+      <div class="linkHeader">Some tagline that we have to come up with</div>
+      <div class="linkButtons">
+        <div class="button">See our Services</div>
+        <div class="button">See our past Work</div>
       </div>
     </div>
 
@@ -30,20 +30,45 @@ module.exports =
   +justify-content(center)
   +align-items(center)
   height: calc(100% + 4px)
-  background-color: #ebebeb
+  position: relative
 
-  .link-container
+  .imageContainer
+    position: absolute
+    top: 0
+    left: 0
+    width: 100%
+    height: 100%
+    background-color: red
+    z-index: -2
+
+  .linkContainer
     background-color: white
+    border: 30px solid rgba(255, 255, 255, 0.5)
     padding: 30px
-    .link-buttons
+    background-clip: padding-box
+    .linkHeader
+      +superHeader(normal)
+      letter-spacing: 1px
+      font-size: 31px
+      line-height: 41px
+      max-width: 450px
+    .linkButtons
       +flexbox
       +align-items(center)
       padding-top: 20px
       .button
         +flex(1)
+        +subHeader(normal)
+        width: 230px
         text-align: center
-        background-color: grey
+        background-color: $contact_background
         color: white
-        padding: 10px 0
+        padding: 15px 0px
+        box-shadow: 6px 6px 0px lightgrey
+        &:first-of-type
+          margin-right: 3px
+        &:last-of-type
+          margin-left: 3px
+
 
 </style>
